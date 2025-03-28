e9d7ff1aaf69fa341a095baf370e3fdf6967f2b6
Bernardo Castellani <becastellani10@gmail.com>
2025-03-28 00:18:06 -0300

    Criação da Autenticação e Login

--- src/app.js
+++ src/app.js
@@ -1,22 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const errorHandler = require('./middlewares/errorHandler');
-const routes = require('./routes/index');
-const AppError = require('./middlewares/appError');
+import express from "express";
+import cors from "cors";
+import routes from "./routes.js";
 
 const app = express();
 
 app.use(cors());
-app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', routes);
+app.use(routes);
 
-app.use((req, res, next) => {
-    next(new AppError('Not Found', 404));
-});
 
-app.use(errorHandler);
-
-module.exports = app;
+export default app;
